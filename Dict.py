b5a505dbbd8c4e0a55f16d48bb9c02b7b0927f27
Jared <jaredjkim@gmail.com>
2017-11-22 03:21:39 -0800

    first commit

---
+++ Dict.py
@@ -0,0 +1,8 @@
+info = {'name': 'Anna', 'Age': 101, 'country of birth': 'The United States', 'favorite language':'Python'}
+for key,data in info.iteritems():
+    print "My", key, "is", data
+
+# print "My name is", dict['Name']
+# print "My age is", dict['Age']
+# print "My country of birth is", dict['Country']
+# print "My favorite language is", dict['Language']
